18af10c3af10d189f8d04d330bb26f315dd414fc
892872518 <34790241+892872518@users.noreply.github.com>
2018-01-31 14:09:57 +0800

    Update EurekaServerApplication.java

--- eureka_server/src/main/java/com/example/demo/EurekaServerApplication.java
+++ eureka_server/src/main/java/com/example/demo/EurekaServerApplication.java
@@ -11,6 +11,5 @@ public class EurekaServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
-		System.out.println("测试提交");
 	}
 }
